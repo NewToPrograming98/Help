610487a238952eb9662c2d3607f47e59371949fb
NewToPrograming98 <Kadenoliver@web.de>
2016-03-19 10:58:15 +0100

    Added files via upload

---
+++ Help/Punkte_1.java
@@ -0,0 +1,9 @@
+import greenfoot.*;
+
+public class Punkte_1 extends Actor
+{
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ Help/Spieler_1.java
@@ -0,0 +1,59 @@
+import greenfoot.*;
+
+public class Spieler_1 extends Actor
+{
+    int Punkte = 0;
+    int punktefeld;
+    public void act() 
+    {
+        Actor spielball = ((Kamele)getWorld()).getSpielball();
+        punktefeld = Spielball.getPunktefeld();
+        if(Greenfoot.isKeyDown("1"))
+        {
+            if(punktefeld == 1)
+            {
+                move(1);
+            }
+            else if(punktefeld == 2)
+            {
+                move(2);
+            }
+            else if(punktefeld == 3)
+            {
+                move(3);
+            }
+            else if(punktefeld == 4)
+            {
+                move(2);
+            }
+            else if(punktefeld == 5)
+            {
+                move(1);
+            }
+            else if(punktefeld == 6)
+            {
+                move(1);
+            }
+            else if(punktefeld == 7)
+            {
+                move(2);
+            }
+            else if(punktefeld == 8)
+            {
+                move(1);
+            }
+            else if(punktefeld == 9)
+            {
+                move(1);
+            }
+            else
+            {
+                move(0);
+            }
+        }
+        else if(Greenfoot.isKeyDown("2"))
+        {
+            move(0);
+        }
+    }    
+}
